2b6c656cb35dfae414df7c77810cc20a2195669f
walacecordeiro <walacecordeirodossantos@gmail.com>
2023-02-08 12:24:39 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+import { Router } from "./router.js"
+
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/exploration", "/pages/exploration.html")
+router.add(404, "/pages/404.html")
+
+router.addBg("/", "url(./images/mountains-universe-1.png)")
+router.addBg("/universe", "url(./images/mountains-universe02.png)")
+router.addBg("/exploration", "url(./images/mountains-universe-3.png)")
+
+router.toReplaceRoutes()
+router.toReplaceBackground()
+
+window.onpopstate = () => router.toReplaceRoutes()
+window.route = () => router.route()
---
+++ js/router.js
@@ -0,0 +1,41 @@
+export class Router {
+    routes = {}
+    background = {}
+
+    add(routeProperty, page){
+        this.routes[routeProperty] = page
+    }
+
+    addBg(routeProperty, bg){
+        this.background[routeProperty] = bg
+    }
+
+    route(event) {
+        event = event || window.event
+        event.preventDefault()
+
+        window.history.pushState({}, "", event.target.href)
+
+        this.toReplaceRoutes()
+        this.toReplaceBackground()
+    }
+
+    toReplaceRoutes() {
+        const { pathname } = window.location
+        const route = this.routes[pathname] || this.routes[404]
+        
+        fetch(route)
+            .then(data => data.text())
+            .then(html => {
+                document.querySelector('#content').innerHTML = html
+            })
+    }
+
+    toReplaceBackground(){
+        const { pathname } = window.location
+        const bg = this.background[pathname]
+        
+        document.querySelector('#body').style.background = bg
+    }
+
+}
